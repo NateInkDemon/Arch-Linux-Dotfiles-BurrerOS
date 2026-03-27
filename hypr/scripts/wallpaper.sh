4cad48b765f903fd5c22410a06e6d771d8a7b2c4
Juan <juanpablohenriquezdelgado307@gmail.com>
2026-03-27 17:13:58 -0400

    wallpaper.sh

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-swww init
-WALLPAPER=$(find ~/Imagenes/Wallpapers -type f | shuf -n 1)
-swww img "$WALLPAPER" --resize crop --transition-type outer --transition-step 90
-wal -i "$WALLPAPER"
+
+if ! pgrep -x "awww-daemon" > /dev/null; then
+    awww-daemon &
+    sleep 0.8
+fi
+
+WALLPAPER=$(find ~/Wallpapers -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
+
+if [ -f "$WALLPAPER" ]; then
+    awww img "$WALLPAPER" --resize crop --transition-type outer --transition-step 60
+
+    wal -i "$WALLPAPER" -n -q
+else
+    echo "Error: No hay wallpapers en ~/Wallpapers"
+fi
